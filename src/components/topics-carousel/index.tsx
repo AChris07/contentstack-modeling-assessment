b734b4e329651615b96e57ac6e9d529efee23295
Cristiano Amici <cristiano.amici@herodigital.com>
2025-05-14 19:20:20 -0500

    Refactor Topics Carousel

--- src/components/topics-carousel/index.tsx
+++ src/components/topics-carousel/index.tsx
@@ -20,37 +20,9 @@ export interface TopicsCarouselProps {
 }
 
 export function TopicsCarousel({ data }: TopicsCarouselProps) {
-  const topics = data?.topicsConnection?.edges?.reduce<TopicSlide[]>(
-    (arr, edge) => {
-      const node = edge?.node;
-      const image = node?.iconConnection?.edges?.[0]?.node;
-
-      if (!node || !image) {
-        return arr;
-      }
-
-      return [
-        ...arr,
-        {
-          title: node.title ?? "",
-          image: image?.url
-            ? {
-                src: image.url,
-                alt: image.description ?? "",
-                width: image.dimension?.width ?? 0,
-                height: image.dimension?.height ?? 0,
-              }
-            : undefined,
-          cta: data?.cta_label
-            ? {
-                label: data?.cta_label ?? "",
-              }
-            : undefined,
-        },
-      ];
-    },
-    []
-  );
+  const topics = data?.topicsConnection?.edges
+    ?.map((edge) => edge?.node)
+    ?.filter((node) => !!node);
 
   if (!topics?.length) {
     return null;
@@ -59,30 +31,34 @@ export function TopicsCarousel({ data }: TopicsCarouselProps) {
   return (
     <div className="mb-4 lg:mb-9 border-b-1 border-secondary-color">
       <TopicsCarouselClient title={data?.title ?? ""}>
-        {topics.map((topic, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center gap-y-6 bg-white rounded-2xl pt-6 px-3 pb-3 lg:px-[30px] lg:pb-[30px]"
-          >
-            {topic.image ? (
-              <Image
-                src={topic.image.src}
-                alt={topic.image.alt}
-                width={topic.image.width}
-                height={topic.image.height}
-                className="w-full h-auto max-w-[100px]"
-              />
-            ) : undefined}
-            <h3 className="font-primary font-bold text-base lg:text-xl text-center">
-              {topic.title}
-            </h3>
-            {topic.cta ? (
-              <button className="font-primary font-bold text-main-color text-xs/2.25 bg-highlight-color rounded-[40px] px-2 lg:px-4 py-1 w-full">
-                {topic.cta.label}
-              </button>
-            ) : undefined}
-          </div>
-        ))}
+        {topics.map((topic, index) => {
+          const image = topic?.iconConnection?.edges?.[0]?.node;
+
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-y-6 bg-white rounded-2xl pt-6 px-3 pb-3 lg:px-[30px] lg:pb-[30px]"
+            >
+              {image?.url ? (
+                <Image
+                  src={image.url}
+                  alt={image.description ?? ""}
+                  width={image.dimension?.width ?? 0}
+                  height={image.dimension?.height ?? 0}
+                  className="w-full h-auto max-w-[100px]"
+                />
+              ) : undefined}
+              <h3 className="font-primary font-bold text-base lg:text-xl text-center">
+                {topic.title}
+              </h3>
+              {data?.cta_label ? (
+                <button className="font-primary font-bold text-main-color text-xs/2.25 bg-highlight-color rounded-[40px] px-2 lg:px-4 py-1 w-full">
+                  {data?.cta_label}
+                </button>
+              ) : undefined}
+            </div>
+          );
+        })}
       </TopicsCarouselClient>
     </div>
   );
